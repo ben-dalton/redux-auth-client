232d8119cd6c622993e11739f796833b899a06ee
ben-dalton <benadalton@gmail.com>
2016-05-23 08:09:46 -0400

    add login error message

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/app';
+import Signin from './components/auth/signin';
 import reducers from './reducers';
 import '../stylesheets/styles';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<Router history={browserHistory}>
+			<Route path="/" component={App}>
+				<Route path="signin" component={Signin} />
+			</Route>
+		</Router>
   </Provider>
   , document.querySelector('.container'));
